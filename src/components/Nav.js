5dd99b90e0b255e356393fb3fc3b7980de2dbad3
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-04-30 23:27:02 +0500

    class14

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,12 +16,13 @@ function Nav() {
             <li><Link to='/'>Products</Link></li>
             <li><Link to='/add'>Add Product</Link></li>
             <li><Link to='/update'>Update Product</Link></li>
-            <li>
-            { auth ? <Link onClick={logout} to='/logout'>Logout</Link> :
-            <Link to='/signup'>Sign Up</Link> }
-            </li>
             <li><Link to='/profile'>Profile</Link></li>
-            <li></li>
+
+            <li>
+            { auth ? <Link onClick={logout} to='/signup'>Logout</Link> :
+            <Link to='/signup'>Sign Up</Link> }
+            
+            </li>
         </ul>
     </div>
   )
